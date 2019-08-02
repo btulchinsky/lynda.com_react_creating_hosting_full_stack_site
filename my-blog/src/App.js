7e707a2b8b4c6978f8eabfe78f10b24958273d39
Barry <barry@BMBP.local>
2019-08-02 00:16:48 -0400

    lesson 01_08 completed

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -6,7 +6,7 @@ import {
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './components/NavBar';
 import './App.css';
 
@@ -27,7 +27,7 @@ class App extends Component {
           <div id="page-body">
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/articles-list" component={ArticlesListPage} />
             <Route path="/article/:name" component={ArticlePage} />
           </div>
         </div>
