96484a79126eb538a5b0e54a6d5ff057a7cee0ff
Barry <barry@BMBP.local>
2019-08-02 00:08:06 -0400

    lesson 01_03 completed

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+const HomePage = () => (
+  <Fragment>
+  <h1>Welcome to my blog!</h1>
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+  <p>Paragraph 3</p>
+  <p>Paragraph 4</p>
+  </Fragment>
+);
+
+export default HomePage;
