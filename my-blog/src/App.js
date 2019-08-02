b9ec89810dca0fd74e2823acfd55155e4e77a516
Barry <barry@BMBP.local>
2019-08-02 00:09:49 -0400

    lesson 01_04 completed

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,6 +4,9 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 import './App.css';
 
 // function App() {
@@ -19,11 +22,12 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route 
-            path="/"
-            component={HomePage}
-            exact
-          />
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/article" component={ArticlePage} />
+          </div>
         </div>
       </Router>
     )
--- my-blog/src/pages/HomePage.js
+++ my-blog/src/pages/HomePage.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
+// import React from 'react';
+// import { Fragment } from 'react';
 
 const HomePage = () => (
   <Fragment>
-  <h1>Welcome to my blog!</h1>
-  <p>Paragraph 1</p>
-  <p>Paragraph 2</p>
-  <p>Paragraph 3</p>
-  <p>Paragraph 4</p>
+    <h1>Welcome to my blog!</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+    <p>Paragraph 3</p>
+    <p>Paragraph 4</p>
   </Fragment>
 );
 
