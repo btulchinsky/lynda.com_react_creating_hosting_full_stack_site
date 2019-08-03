b98b88de8f33df3a505fc2e0dfc9d8ce5219ad68
Barry <barry@BMBP.local>
2019-08-02 22:48:26 -0400

    lesson 02_07 completed

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,13 +3,16 @@ import bodyParser from 'body-parser';
 
 const articleInfo = {
   'learn-react': {
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
   },
   'learn-node': {
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
   },
   'my-thoughts-on-resumes': {
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
   }
 }
 
@@ -21,6 +24,15 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
   articleInfo[articleName].upvotes += 1;
   res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes!`);
-})
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articleInfo[articleName].comments.push({ username, text });
+
+  res.status(200).send(articleInfo[articleName]);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
