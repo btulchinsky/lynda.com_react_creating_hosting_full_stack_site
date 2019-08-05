8133d36d5d2cb92326280f76b4df1370f2bee6ed
Barry <barry@BMBP.local>
2019-08-05 19:30:47 -0400

    lesson 05_01 completed

---
+++ my-blog-backend/src/build/precache-manifest.d880a04ba50895d135ad7ad17bee3c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cf082364c6a465ecf159a4654f5dfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24b904f92b14e762be4",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "eb73c713cc08bfd952c6",
+    "url": "/static/js/2.da4d33e8.chunk.js"
+  },
+  {
+    "revision": "a24b904f92b14e762be4",
+    "url": "/static/js/main.d6aa1480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
